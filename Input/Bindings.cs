9baf4c41aeb28048d4f9ae34be816991d956118b
Dave Cousineau <sahuagin@live.ca>
2021-02-04 21:11:51 -0700

    refactor three binding classes seal, make members private, readonly, etc. implement IEnumerable on Bindings copy constructor for cloning return strongly typed clone create strongly typed Add overloads in Bindings

--- Input/Bindings.cs
+++ Input/Bindings.cs
@@ -1,74 +1,79 @@
-﻿using Microsoft.Xna.Framework.Input;
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using Microsoft.Xna.Framework.Input;
 
-namespace DownUnder.Input
-{
-    /// <summary>
-    /// A list of "Binding"s, one for each action.
-    /// </summary>
-    [Serializable()]
-    public class Bindings
-    {
-        public List<Binding> bindings = new List<Binding>();
+namespace DownUnder.Input {
+    /// <summary> A list of "Binding"s, one for each action. </summary>
+    [Serializable]
+    public sealed class Bindings : IEnumerable<Binding> {
+        readonly List<Binding> _bindings = new List<Binding>();
 
-        public Bindings()
-        {
-            int actions = Enum.GetNames(typeof(ActionType)).Length;
-            for (int i = 0; i < actions; i++)
-            {
-                bindings.Add(new Binding());
-            }
+        public Binding this[int index] { get => _bindings[index]; set => _bindings[index] = value; }
+
+        public Bindings() {
+            var numActions = Enum.GetNames(typeof(ActionType)).Length;
+            for (var i = 0; i < numActions; i++)
+                _bindings.Add(new Binding());
         }
 
-        public void Set(List<Binding> binding_)
-        {
-            bindings = binding_;
-        }
+        Bindings(Bindings source) =>
+            _bindings = source._bindings.ToList();
 
-        public List<Binding> ToList()
-        {
-            return bindings;
-        }
+        public IEnumerator<Binding> GetEnumerator() => _bindings.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public void AddCombo(int input_enum, ActionType action_type)
-        {
-            Binding binding = new Binding();
-            binding.buttons_combo.Add(input_enum);
+        public void AddCombo(
+            int input_enum,
+            ActionType action_type
+        ) {
+            var binding = new Binding();
+            binding.AddCombo(input_enum);
             Add(binding, action_type);
         }// --vvvv
 
-        public void Add(int input_enum, ActionType action_type)
-        {
-            Binding binding = new Binding();
-            binding.buttons.Add(input_enum);
+        public void Add(
+            Buttons button,
+            ActionType action_type
+        ) =>
+            Add((int)button, action_type);
+
+        public void Add(
+            Keys keys,
+            ActionType action_type
+        ) =>
+            Add((int)keys, action_type);
+
+        void Add(
+            int input_enum,
+            ActionType action_type
+        ) {
+            var binding = new Binding();
+            binding.AddButton(input_enum);
             Add(binding, action_type);
         }// --v
 
-        public void Add(Binding binding, ActionType action_type)
-        {
-            bindings[(int)action_type] += (Binding)binding.Clone();
-        }// ---
+        public void Add(
+            Binding binding,
+            ActionType action_type
+        ) =>
+            _bindings[(int)action_type] += binding;
+        // ---
 
-        public object Clone()
-        {
-            Bindings clone = new Bindings
-            {
-                bindings = bindings.GetRange(0, bindings.Count)
-            };
-            return clone;
-        }
+        public Bindings Clone() =>
+            new Bindings(this);
 
-        public static Bindings DefaultBindings(ControllerType controller_type)
-        {
-            switch (controller_type)
-            {
+        public static Bindings DefaultBindings(
+            ControllerType controller_type
+        ) {
+            switch (controller_type) {
                 case ControllerType.keyboard:
-                    return DefaultKeyboardBindings();
+                    return GetDefaultKeyboardBindings();
 
                 case ControllerType.xbox:
-                    return Default360Bindings();
+                    return GetDefault360Bindings();
 
                 default:
                     Debug.WriteLine("Bindings.DefaultBindings: Unimplemented controller type.");
@@ -76,59 +81,52 @@ namespace DownUnder.Input
             }
         }
 
-        private static Bindings Default360Bindings()
-        {
-            Bindings bindings = new Bindings();
-            bindings.Add((int)Buttons.A, ActionType.menu_select);
-            bindings.Add((int)Buttons.Start, ActionType.menu_select);
-            bindings.Add((int)Buttons.B, ActionType.menu_back);
+        static Bindings GetDefault360Bindings() =>
+            new Bindings {
+                { Buttons.A, ActionType.menu_select },
+                { Buttons.Start, ActionType.menu_select },
+                { Buttons.B, ActionType.menu_back },
 
-            bindings.Add((int)Buttons.DPadUp, ActionType.camera_zoom_in);
-            bindings.Add((int)Buttons.DPadDown, ActionType.camera_zoom_out);
+                { Buttons.DPadUp, ActionType.camera_zoom_in },
+                { Buttons.DPadDown, ActionType.camera_zoom_out },
 
-            bindings.Add((int)Buttons.LeftThumbstickLeft, ActionType.menu_select_left);
-            bindings.Add((int)Buttons.LeftThumbstickRight, ActionType.menu_select_right);
-            bindings.Add((int)Buttons.LeftThumbstickUp, ActionType.menu_select_up);
-            bindings.Add((int)Buttons.LeftThumbstickDown, ActionType.menu_select_down);
+                { Buttons.LeftThumbstickLeft, ActionType.menu_select_left },
+                { Buttons.LeftThumbstickRight, ActionType.menu_select_right },
+                { Buttons.LeftThumbstickUp, ActionType.menu_select_up },
+                { Buttons.LeftThumbstickDown, ActionType.menu_select_down },
 
-            bindings.Add((int)Buttons.RightThumbstickLeft, ActionType.menu_select_left);
-            bindings.Add((int)Buttons.RightThumbstickRight, ActionType.menu_select_right);
-            bindings.Add((int)Buttons.RightThumbstickUp, ActionType.menu_select_up);
-            bindings.Add((int)Buttons.RightThumbstickDown, ActionType.menu_select_down);
+                { Buttons.RightThumbstickLeft, ActionType.menu_select_left },
+                { Buttons.RightThumbstickRight, ActionType.menu_select_right },
+                { Buttons.RightThumbstickUp, ActionType.menu_select_up },
+                { Buttons.RightThumbstickDown, ActionType.menu_select_down },
 
-            bindings.Add((int)Buttons.LeftThumbstickLeft, ActionType.left_movement);
-            bindings.Add((int)Buttons.LeftThumbstickRight, ActionType.right_movement);
-            bindings.Add((int)Buttons.LeftThumbstickUp, ActionType.up_movement);
-            bindings.Add((int)Buttons.LeftThumbstickDown, ActionType.down_movement);
+                { Buttons.LeftThumbstickLeft, ActionType.left_movement },
+                { Buttons.LeftThumbstickRight, ActionType.right_movement },
+                { Buttons.LeftThumbstickUp, ActionType.up_movement },
+                { Buttons.LeftThumbstickDown, ActionType.down_movement },
 
-            bindings.Add((int)Buttons.RightTrigger, ActionType.menu_select_speed_modifier);
-            bindings.Add((int)Buttons.LeftTrigger, ActionType.menu_select_speed_modifier);
-            return bindings;
-        }
+                { Buttons.RightTrigger, ActionType.menu_select_speed_modifier },
+                { Buttons.LeftTrigger, ActionType.menu_select_speed_modifier }
+            };
 
-        private static Bindings DefaultKeyboardBindings()
-        {
-            Bindings bindings = new Bindings();
-            bindings.Add((int)Keys.Enter, ActionType.menu_select);
-
-            bindings.Add((int)Keys.Left, ActionType.left_movement);
-            bindings.Add((int)Keys.Right, ActionType.right_movement);
-            bindings.Add((int)Keys.Up, ActionType.up_movement);
-            bindings.Add((int)Keys.Down, ActionType.down_movement);
-
-            bindings.Add((int)Keys.Enter, ActionType.menu_select);
-            bindings.Add((int)Keys.Space, ActionType.menu_select);
-            bindings.Add((int)Keys.Left, ActionType.menu_select_left);
-            bindings.Add((int)Keys.Right, ActionType.menu_select_right);
-            bindings.Add((int)Keys.Up, ActionType.menu_select_up);
-            bindings.Add((int)Keys.Down, ActionType.menu_select_down);
-
-            bindings.Add((int)Keys.A, ActionType.menu_select_left);
-            bindings.Add((int)Keys.D, ActionType.menu_select_right);
-            bindings.Add((int)Keys.W, ActionType.menu_select_up);
-            bindings.Add((int)Keys.S, ActionType.menu_select_down);
-            bindings.Add((int)Keys.LeftShift, ActionType.menu_select_speed_modifier);
-            return bindings;
-        }
+        static Bindings GetDefaultKeyboardBindings() =>
+            new Bindings {
+                { Keys.Enter, ActionType.menu_select },
+                { Keys.Left, ActionType.left_movement },
+                { Keys.Right, ActionType.right_movement },
+                { Keys.Up, ActionType.up_movement },
+                { Keys.Down, ActionType.down_movement },
+                { Keys.Enter, ActionType.menu_select },
+                { Keys.Space, ActionType.menu_select },
+                { Keys.Left, ActionType.menu_select_left },
+                { Keys.Right, ActionType.menu_select_right },
+                { Keys.Up, ActionType.menu_select_up },
+                { Keys.Down, ActionType.menu_select_down },
+                { Keys.A, ActionType.menu_select_left },
+                { Keys.D, ActionType.menu_select_right },
+                { Keys.W, ActionType.menu_select_up },
+                { Keys.S, ActionType.menu_select_down },
+                { Keys.LeftShift, ActionType.menu_select_speed_modifier }
+            };
     }
 }
--- Input/BindingsSet.cs
+++ Input/BindingsSet.cs
@@ -1,72 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace DownUnder.Input
-{
-    /// <summary>
-    /// A list of "Bindings"s, one for each controller.
-    /// </summary>
-    [Serializable()]
-    public class BindingsSet
-    {
-        public List<Bindings> bindings_set = new List<Bindings>();
+namespace DownUnder.Input {
+    /// <summary> A list of "Bindings"s, one for each controller. </summary>
+    [Serializable]
+    public sealed class BindingsSet {
+        readonly List<Bindings> _bindings_set = new List<Bindings>();
 
-        public BindingsSet()
-        {
-            int actions = Enum.GetNames(typeof(ControllerType)).Length;
-            for (int i = 0; i < actions; i++)
-            {
-                bindings_set.Add(new Bindings());
-            }
+        public BindingsSet() {
+            var numActions = Enum.GetNames(typeof(ControllerType)).Length;
+
+            for (var i = 0; i < numActions; i++)
+                _bindings_set.Add(new Bindings());
+
             SetDefaults();
         }
 
-        /// <summary>
-        /// This will have to be (and stay) here.
-        /// </summary>
-        public void SetDefaults()
-        {
-            int controllers = Enum.GetNames(typeof(ControllerType)).Length;
-            for (int i = 0; i < controllers; i++)
-            {
-                SetBindings((ControllerType)i, Bindings.DefaultBindings((ControllerType)i));
+        /// <summary> This will have to be (and stay) here. </summary>
+        public void SetDefaults() {
+            var numControllers = Enum.GetNames(typeof(ControllerType)).Length;
+
+            for (var i = 0; i < numControllers; i++) {
+                var controllerType = (ControllerType)i;
+                SetBindings(controllerType, Bindings.DefaultBindings(controllerType));
             }
         }
 
-        public Binding GetBinding(Controller controller, ActionType action_type)
-        {
-            return bindings_set[(int)controller.type].bindings[(int)action_type];
-        }
+        public Binding GetBinding(
+            Controller controller,
+            ActionType action_type
+        ) =>
+            _bindings_set[(int)controller.type][(int)action_type];
 
-        public void SetBinding(ControllerType controller_type, ActionType action_type, Binding binding)
-        {
-            bindings_set[(int)controller_type].bindings[(int)action_type] = (Binding)binding.Clone();
-        }
+        public void SetBinding(
+            ControllerType controller_type,
+            ActionType action_type,
+            Binding binding
+        ) =>
+            _bindings_set[(int)controller_type][(int)action_type] = binding.Clone();
 
-        public void SetBindings(ControllerType controller_type, Bindings bindings)
-        {
-            bindings_set[(int)controller_type] = (Bindings)bindings.Clone();
-        }
+        public void SetBindings(
+            ControllerType controller_type,
+            Bindings bindings
+        ) =>
+            _bindings_set[(int)controller_type] = bindings.Clone();
 
-        public PseudoButton GetAction(InputState input_state, Controller controller, ActionType action_type)
-        {
-            Binding binding = GetBinding(controller, action_type);
+        public PseudoButton GetAction(
+            InputState input_state,
+            Controller controller,
+            ActionType action_type
+        ) {
+            var binding = GetBinding(controller, action_type);
 
-            PseudoButton buttons_result = new PseudoButton();
-            for (int i = 0; i < binding.buttons.Count; i++)
-            {
-                buttons_result |= DirectInput.GetPressedButton(controller, binding.buttons[i], input_state);
-            }
+            var buttons_result = new PseudoButton();
+            for (var i = 0; i < binding.NumButtons; i++)
+                buttons_result |= DirectInput.GetPressedButton(controller, binding.GetButton(i), input_state);
 
-            PseudoButton combo_result = new PseudoButton();
-            if (binding.buttons_combo.Count > 0)
-            {
-                combo_result = new PseudoButton(true, 1f, true);
-                for (int i = 0; i < binding.buttons.Count; i++)
-                {
-                    combo_result &= DirectInput.GetPressedButton(controller, binding.buttons[i], input_state);
-                }
-            }
+            if (binding.NumCombos <= 0)
+                return buttons_result | new PseudoButton(); // TODO: does ORing here do nothing?
+
+            var combo_result = new PseudoButton(true, 1f, true);
+            for (var i = 0; i < binding.NumButtons; i++)
+                combo_result &= DirectInput.GetPressedButton(controller, binding.GetButton(i), input_state);
 
             return buttons_result | combo_result;
         }
